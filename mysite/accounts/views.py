56894bf585ae417632e45383b9cbe2cead9925cb
Ruud Withagen <ruud.withagen@gmail.com>
2012-02-13 22:04:45 +0100

    use template instead of static response

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -1,8 +1,9 @@
 from django.http import HttpResponse
 from django.shortcuts import render_to_response, get_object_or_404
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import login as authlogin
 
 @login_required
 def profile(request):
-    return HttpResponse("You are logged in!!")
-    
+    return render_to_response('user_logged_in.html', {'user':request.user})
+
--- mysite/urls.py
+++ mysite/urls.py
@@ -5,15 +5,13 @@ from django.contrib.auth.views import login, logout
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^polls/$', 'polls.views.index'),
-    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
-    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
-    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+#    url(r'^polls/$', 'polls.views.index'),
+#    url(r'^polls/(?P<poll_id>\d+)/$', 'polls.views.detail'),
+#    url(r'^polls/(?P<poll_id>\d+)/results/$', 'polls.views.results'),
+#    url(r'^polls/(?P<poll_id>\d+)/vote/$', 'polls.views.vote'),
+    url(r'^bencotto', login),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^accounts/login/$', login),
     url(r'^accounts/logout/$', logout),
     url(r'^accounts/profile/$', 'accounts.views.profile')
 )
 admin.autodiscover()
-
-
